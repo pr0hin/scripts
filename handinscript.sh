ce9d2ec11b9d505fcc52779c8675444b3e515aed
r0e <rohinjpatel@gmail.com>
2014-08-09 13:18:05 -0700

    removed unneeded code

--- handinscript.sh
+++ handinscript.sh
@@ -1,5 +1,5 @@
-#!bin/bash -x
-QUIT="Quit this program"
+#!bin/bash 
+
 while true; do
     echo  "Enter CS ID:"
     read ID
@@ -36,15 +36,11 @@ while true; do
             break
         fi
     done
+    
     echo  "Enter the destination folder. This will also be used as the first argument to handin, usually the course number (Will be created in the home directory if it doesn't exist)" 
     read DEST
     echo  "Enter assignment/lab folder. This will be the second argument to handin, usually the assignment/lab folder"
     read ASSIGN
-
-
-    #if [ "${ARG:0:1}" == "~" ]; then
-    #ARG=${HOME}${ARG:1}
-    #fi
 
     if [ -d "$ARG" ]; then
         scp -r "$ARG" "$ID"@remote.ugrad.cs.ubc.ca:~/$DEST 
